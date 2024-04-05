4bb791420f966b7baa7ad5720bc187352f1cf108
Vladislav Navrocky <navrocky.vlad@gmail.com>
2024-04-05 18:47:03 +0300

    Fix install script

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 set -e
-set -x
 
-curl -L https://github.com/navrocky/dcw/releases/download/1.1.0/dcw -o /usr/local/bin/dcw
+curl -fL https://github.com/navrocky/dcw/releases/download/1.1.0/dcw -o /usr/local/bin/dcw
 chmod +x /usr/local/bin/dcw
-curl -L https://github.com/navrocky/dcw/raw/master/completion.bash -o /etc/bash_completion.d/dcw_completion.bash
+curl -fL https://github.com/navrocky/dcw/raw/master/completion.bash -o /etc/bash_completion.d/dcw_completion.bash
 
 echo "== Installation completed successfully =="
